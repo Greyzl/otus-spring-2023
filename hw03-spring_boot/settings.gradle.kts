4cdca7f5b2a6cc2a3c7f092ee4d6f6e0b30b3f22
Шилков Сергей Сергеевич <sshilkov@alfabank.ru>
2023-05-10 19:19:22 +0300

    Добавил зависимости через стартеры. Перевел мейн на @SpringBootApplication

--- hw03-spring_boot/settings.gradle.kts
+++ hw03-spring_boot/settings.gradle.kts
@@ -1,9 +1,11 @@
-rootProject.name = "hw03-config"
+rootProject.name = "hw03-spring_boot"
 
 pluginManagement{
     val johnrengelmanShadow = "7.1.2"
+    val dependencyManagement = "1.1.0"
     plugins {
         id("com.github.johnrengelman.shadow") version johnrengelmanShadow
+        id("io.spring.dependency-management") version dependencyManagement
     }
 }
 
