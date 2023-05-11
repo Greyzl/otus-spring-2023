155fb548beb034e9d52cd80037e9672e549f3735
Шилков Сергей Сергеевич <sshilkov@alfabank.ru>
2023-05-11 21:38:01 +0300

    Поправил тесты

--- hw03-spring_boot/src/main/java/ru/otus/Main.java
+++ hw03-spring_boot/src/main/java/ru/otus/Main.java
@@ -3,10 +3,14 @@ package ru.otus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.otus.service.ApplicationRunner;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        ApplicationContext context = SpringApplication.run(Main.class, args);
+        ApplicationRunner runner = context.getBean(ApplicationRunner.class);
+        runner.run();
     }
 }
